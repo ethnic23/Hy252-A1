15c848b80d1a31a90b3be8a3c7dd23f41cf5f959
mikes <csd4738@csd.uoc.gr>
2021-10-18 13:20:04 +0300

    Changed M and L to main parameters, added graphics method

--- src/A11/DrawF.java
+++ src/A11/DrawF.java
@@ -4,33 +4,44 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 import java.io.*;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.QuadCurve2D;
 
 public class DrawF {
     public static void main(String[] args) throws Exception {
-        char M;
-        int L;
-        Scanner in = new Scanner(System.in);
+        String M=args[0];
+        int L=Integer.parseInt(args[1]);
+        while(L>=3&&L<=20) {
+            Scanner in = new Scanner(System.in);
 
-        System.out.println("Give a character for M: ");
-        M = in.next().charAt(0);
-        System.out.println("Give an integer for L: ");
-        L = in.nextInt();
+            if (M.equals("w") ){
+                L = Integer.parseInt(JOptionPane.showInputDialog("Dwse enan ari8mo gia to L:",3));
+            } else {
+                System.out.println("Give an integer for L: ");
+                L = in.nextInt();
+            }
+            if (M.equals("c")&&M.equals("w")&&M.equals("f")&&M.equals("g")) {
+                throw new Exception("Not permitted input for M");
+            } else if (L < 3 || L > 20) {
+                throw new Exception("Not permitted input for L");
+            }
 
-        if ((M != 'c') && (M != 'w') && (M != 'f') && (M != 'g')) {
-            throw new Exception("Not permitted input for M");
-        } else if (L < 3 || L > 20) {
-            throw new Exception("Not permitted input for L");
-        }
-        if (M == 'c') {
-            console(M, L);
-        } else if (M == 'w') {
-            window(M, L);
-        }else if(M =='f'){
-            file(M,L);
+            if (M.equals("c")) {
+                console(L);
+            } else if (M.equals("w")) {
+                window(L);
+            } else if (M.equals("f")) {
+                file(L);
+            } else if (M.equals("g")) {
+                graphics(L);
+            }
         }
     }
 
-    private static void console(char M, int L) {
+    private static void console(int L) {
         String ast = "*";
         for (int i = 0; i < L; i++) {
             if (i == 0) {
@@ -49,7 +60,7 @@ public class DrawF {
 
         }
     }
-    private static void window(char M,int L) {
+    private static void window(int L) {
         String ast = "*";
         String str = "";
         for (int i = 0; i < L; i++) {
@@ -69,7 +80,7 @@ public class DrawF {
         }
         JOptionPane.showMessageDialog(null,str,"Παραθυρο Εξοδου",JOptionPane.INFORMATION_MESSAGE);
     }
-    private static void file(char M,int L) throws FileNotFoundException, UnsupportedEncodingException {
+    private static void file(int L) throws FileNotFoundException, UnsupportedEncodingException {
         try {
             File Ffile = new File("C:\\temp\\F.html");
             String data = "<!DOCTYPE html>\n" +
@@ -87,7 +98,17 @@ public class DrawF {
         }
 
     }
-    private static void graphics(char M,int L){
+    private static void graphics(int L){
+        Frame f = new Frame("Zwgrafizontas to F"){
+            public void paint (Graphics g){
+                Graphics2D g2 = (Graphics2D) g;
+                g2.draw(new Line2D.Double(50,30,30,50+L*10));
+                g2.draw(new QuadCurve2D.Double(30,50,30,70,40+L*5,50));
+                g2.draw(new QuadCurve2D.Double(20,100,40,120,40+L*5,90));
+            }
 
+        };
+        f.setSize(400,400);
+        f.setVisible(true);
     }
 }
